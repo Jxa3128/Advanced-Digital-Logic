43e0fe0a42b7098ab18317d70c3da93d240c1208
Jorge Avila <jorge.avila@mavs.uta.edu>
2021-02-06 12:46:07 -0600

    fixed KNight

--- Knight Rider Flasher/KnightRiderFlasher.v
+++ Knight Rider Flasher/KnightRiderFlasher.v
@@ -17,82 +17,140 @@ module KnightRiderFlasher(
 	
 	
 	
-	input OnOff,
-	input ClockKey, output [9:0] LEDRArray);
-	
+	input OnOff, //key1
+	input ClockKey, //clock_50
+	output [9:0] LEDRArray); //the 10 LEDR
 
+
+	//input wires from the schematic
+	wire divisor;
+	wire en;
+	wire [3:0] bufferCounter;
+	
+	ToggleLatch tl (OnOff, ClockKey, en);
+	
+	//CLOCK_50 && Divisor Input 
+	// this tells me if it is a 1 or a 0
+	//since we need an output every 10 clocks
+	
+	//call it 3 diff times for the report
+	divideX4_5 fn1 (ClockKey,divisor);
+	//divideX9 fn1 (ClockKey,divisor);
+	//divideX18 fn1 (ClockKey,divisor);
+	
+	
+	//decade counter is called
+	DecadeUpDown DU1 (divisor, OnOff, leftRight, bufferCounter);
 	
 endmodule
 
-module BinaryUpDown(
+//lets assume this bit is right
+module DecadeUpDown(
 	
-	input CLK,CLEAR, UP,
+	input CLK, UP,
 	output reg [N-1:0] COUNT);
-	parameter N = 3'd3;
-		always @ (posedge CLK, negedge CLEAR)
-			if(CLEAR == 0) COUNT <= 0; //clear is active low
-			else 
+	parameter N = 4'd10;
+		always @ (posedge CLK)
 				if(UP == 0)
-					COUNT <= COUNT + 1'b1;
+					COUNT <= COUNT + 4'd1;
 				else 
-					COUNT <= COUNT + 1'b1;
+					COUNT <= COUNT + 4'd1;
 					
 endmodule
 
 
 //OnOff Toggle Latch. Assumes an normally-on push button switch for OnOff.
 module ToggleLatch ( 
-	input OnOff, IN, CLR,
+	input OnOff, IN,
 	output OUT);
+	
 	reg state, nextstate;
-	parameter ON=1'b1, OFF=1'b0;
-		always @ (negedge OnOff, negedge CLR) //Active-low CLR.
-			if (CLR==1'b0) state <= OFF; else state <= nextstate;
-				always @ (state) 
-					case(state)
-						OFF: nextstate = ON;
-						ON: nextstate = OFF;
-						//CLR turns the switch off.
-						//Pushing OnOff turns the switch on. //Pushing OnOff turns the switch off.
+	parameter ON= 1, OFF= 0;
+	
+		always @ (negedge OnOff)
+			state <= nextstate;
+		always @ (state) 
+				case(state)
+					OFF: nextstate = ON;
+					ON: nextstate = OFF;
+					//CLR turns the switch off.
+					//Pushing OnOff turns the switch on. //Pushing OnOff turns the switch off.
 					endcase
 	assign OUT = state*IN; //Out = In when switch in on. Otherwise, Out = 0.
 	
 endmodule
 
+//twice per second
+module divideX4_5 ( 
+	
+	input CLK,
+	output reg OUT);
+	parameter N= 2500000; //(50e6/10LED)/2
+		reg [31:0] COUNT;
+		always @ (negedge CLK)
+			begin
+			if (COUNT == N-2) 
+				begin 
+					OUT <= 1; 
+					COUNT <= N-1; end 
+			else
+			if (COUNT == N-1) 
+				begin 
+					OUT <=1'b0; 
+					COUNT <= 0; end 
+			else 
+				begin 
+					OUT <= 0; 
+					COUNT <= COUNT + 1; end 
+			end 
+endmodule 
 
-// One/Ninth across 50e6/9 => 55,555,555.55555555
+// once per second
 module divideX9 ( 
 	
-	input CLK, CLEAR,
-	output reg [9:0] COUNT,
+	input CLK,
 	output reg OUT);
-	parameter N=10'd55555555;
-		always @ (negedge CLK, negedge CLEAR)
-			if (CLEAR==1'b0) COUNT <= 10'b0; 
-			else
+	parameter N= 5000000; //(50e6)/10LEDR
+		reg [31:0] COUNT;
+		always @ (negedge CLK)
 			begin
-				if (COUNT == N-2'd2) begin OUT <= 1'b1; COUNT <= N-1'd1; end // Once COUNT = N-2 OUT = 1
+			if (COUNT == N-2) 
+				begin 
+					OUT <= 1; 
+					COUNT <= N-1; end 
 			else
-			if (COUNT == N-1'd1) begin OUT <=1'b0; COUNT <= 10'b0; end //Once COUNT = N-1 OUT=0
-				else begin OUT <= 1'b0; COUNT <= COUNT + 1'b1; end // COUNT is incremented
+			if (COUNT == N-1) 
+				begin 
+					OUT <=1'b0; 
+					COUNT <= 0; end 
+			else 
+				begin 
+					OUT <= 0; 
+					COUNT <= COUNT + 1; end 
 			end 
 endmodule 
 
-// One/2*Ninth across 50e6/18 => 27,777,777.777777776
+// once per twice a second
 module divideX18 ( 
 	
-	input CLK, CLEAR,
-	output reg [9:0] COUNT,
+	input CLK,
 	output reg OUT);
-	parameter N=10'd27777777;
-		always @ (negedge CLK, negedge CLEAR)
-			if (CLEAR==1'b0) COUNT <= 10'b0; 
-			else
+	parameter N= 100000000; //(50e6/10LED)*2
+		reg [31:0] COUNT;
+		always @ (negedge CLK)
 			begin
-				if (COUNT == N-2'd2) begin OUT <= 1'b1; COUNT <= N-1'd1; end // Once COUNT = N-2 OUT = 1
+			if (COUNT == N-2) 
+				begin 
+					OUT <= 1; 
+					COUNT <= N-1; end 
 			else
-			if (COUNT == N-1'd1) begin OUT <=1'b0; COUNT <= 10'b0; end //Once COUNT = N-1 OUT=0
-				else begin OUT <= 1'b0; COUNT <= COUNT + 1'b1; end // COUNT is incremented
+			if (COUNT == N-1) 
+				begin 
+					OUT <=1'b0; 
+					COUNT <= 0; end 
+			else 
+				begin 
+					OUT <= 0; 
+					COUNT <= COUNT + 1; end 
 			end 
 endmodule 
-
